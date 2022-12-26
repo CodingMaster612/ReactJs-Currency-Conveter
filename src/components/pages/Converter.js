b7c5f8ca7ac5a71b30adf7a94b1a52cf0f21121b
CodingMaster612 <105937944+CodingMaster612@users.noreply.github.com>
2022-12-25 20:46:58 -0600

    test

--- src/components/pages/Converter.js
+++ src/components/pages/Converter.js
@@ -2,12 +2,37 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 import '../../App.css'
-import BackgroundImage from '../../assets/images/sleep.png'
+import BackgroundImage from '../../assets/images/pink2.png'
 
 export default function Converter() {
     return (
-        <header style={ HeaderStyle }>
-            
+        <header style={HeaderStyle}>
+
+            <div className="white-box">
+                <div class="tab">
+                    <button class="tablinks" onclick="openCity(event, 'London')">London</button>
+                    <button class="tablinks" onclick="openCity(event, 'Paris')">Paris</button>
+                    <button class="tablinks" onclick="openCity(event, 'Tokyo')">Tokyo</button>
+                </div>
+
+                <div id="London" class="tabcontent">
+                    <h3>London</h3>
+                    <p>London is the capital city of England.</p>
+                </div>
+
+                <div id="Paris" class="tabcontent">
+                    <h3>Paris</h3>
+                    <p>Paris is the capital of France.</p>
+                </div>
+
+                <div id="Tokyo" class="tabcontent">
+                    <h3>Tokyo</h3>
+                    <p>Tokyo is the capital of Japan.</p>
+                </div>
+            </div>
+
+
+
         </header>
     )
 }

4fb125c6fdfebc95cfc08aeea4c34aa3752c47f2
CodingMaster612 <105937944+CodingMaster612@users.noreply.github.com>
2022-12-25 22:06:04 -0600

    test

--- src/components/pages/Converter.js
+++ src/components/pages/Converter.js
@@ -10,16 +10,21 @@ export default function Converter() {
         <header style={HeaderStyle}>
 
             <div className="white-box">
-               <Tabs />
+                <Tabs />
 
-                
+
             </div>
 
-        
 
-            <div>
-     
-    </div>
+
+
+            <div class="topnav">
+                <a class="active" href="#home">Home</a>
+                <a href="#news">News</a>
+                <a href="#contact">Contact</a>
+                <a href="#about">About</a>
+            </div>
+
 
 
 
@@ -27,7 +32,7 @@ export default function Converter() {
 
         </header>
 
-        
+
     )
 }
 
